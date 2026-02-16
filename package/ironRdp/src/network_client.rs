54265651f7b36e21a0c6035ff9d3e74c55a2551c
Luca Rickli <luca@rickli.dev>
2026-02-16 11:34:58 +0100

    refactor: move 3rd party code to packages

---
+++ package/ironRdp/src/network_client.rs
@@ -0,0 +1,59 @@
+// Copyright (c) Devolutions & contributors
+// GitHub: Devolutions/IronRDP
+// Source: https://github.com/Devolutions/IronRDP/blob/bdde2c76ded7315f7bc91d81a0909a1cb827d870/crates/ironrdp-web/src/network_client.rs
+
+use core::pin::Pin;
+
+use futures_util::Future;
+use ironrdp::connector::sspi::generator::NetworkRequest;
+use ironrdp::connector::sspi::network_client::NetworkProtocol;
+use ironrdp::connector::{custom_err, reason_err, ConnectorResult};
+use ironrdp_futures::AsyncNetworkClient;
+
+#[derive(Debug)]
+pub(crate) struct WasmNetworkClient;
+
+impl AsyncNetworkClient for WasmNetworkClient {
+    fn send<'a>(
+        &'a mut self,
+        network_request: &'a NetworkRequest,
+    ) -> Pin<Box<dyn Future<Output = ConnectorResult<Vec<u8>>> + 'a>> {
+        Box::pin(async move {
+            debug!(?network_request.protocol, ?network_request.url);
+
+            match &network_request.protocol {
+                NetworkProtocol::Http | NetworkProtocol::Https => {
+                    let body = js_sys::Uint8Array::from(&network_request.data[..]);
+
+                    let response = gloo_net::http::Request::post(network_request.url.as_str())
+                        .header("keep-alive", "true")
+                        .body(body)
+                        .map_err(|e| custom_err!("failed to send KDC request", e))?
+                        .send()
+                        .await
+                        .map_err(|e| custom_err!("failed to send KDC request", e))?;
+
+                    if !response.ok() {
+                        return Err(reason_err!(
+                            "KdcProxy",
+                            "HTTP status error ({} {})",
+                            response.status(),
+                            response.status_text(),
+                        ));
+                    }
+
+                    let body = response
+                        .binary()
+                        .await
+                        .map_err(|e| custom_err!("failed to retrieve HTTP response", e))?;
+
+                    Ok(body)
+                }
+                unsupported => Err(reason_err!(
+                    "CredSSP",
+                    "unsupported protocol: {unsupported:?}"
+                )),
+            }
+        })
+    }
+}
